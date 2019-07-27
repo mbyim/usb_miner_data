7d3957a3104730e898661f5ec97b23bb624327fc
martin <mbyim@bu.edu>
2019-07-27 18:43:45 -0400

    initial commit

---
+++ pool_data.py
@@ -0,0 +1,31 @@
+import pandas
+from sqlalchemy import create_engine
+import json
+import time
+import sqlite3
+import os
+
+WALLET_ADDRESS = os.environ['WALLET_ADDRESS']
+POOL_URL = "ckpool.com"
+TABLE_NAME = "pool_data"
+
+
+
+
+def get_pool_data():
+
+
+
+def clean_hashrate():
+
+
+
+def insert_data():
+
+
+def main():
+
+
+if __name__='__main__':
+	main()
+
