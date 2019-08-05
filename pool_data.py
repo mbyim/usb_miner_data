f46cd27eb988e6a899f2aaa567beedfd10503cfe
martin <mbyim@bu.edu>
2019-08-04 22:04:40 -0400

    adding shares script

--- pool_data.py
+++ pool_data.py
@@ -25,8 +25,8 @@ def get_pool_data():
 	return tstamp, json_data 
 
 
-#def clean_hashrate(hashrate_txt):
-	fixed = float(hashrate_txt[:-1]
+def clean_hashrate(hashrate_txt):
+	fixed = float(hashrate_txt[:-1])
 	if 'K' in hashrate_txt:
 		return fixed*1000
 	elif 'M' in hashrate_txt:
@@ -45,12 +45,14 @@ def insert_data(tstamp, json_data):
 	df['tstamp'] = tstamp
 	#Clean df
 	df[['hashrate1m', 'hashrate5m', 'hashrate1hr', 'hashrate1d', 'hashrate7d']] = \
-		df[['hashrate1m', 'hashrate5m', 'hashrate1hr', 'hashrate1d', 'hashrate7d']] \ 
+		df[['hashrate1m', 'hashrate5m', 'hashrate1hr', 'hashrate1d', 'hashrate7d']] \
 			.applymap(lambda x: clean_hashrate(x))
+	df['worker'] = df['worker'].astype(str)
 	#create table if not exists
 	conn = sqlite3.connect(DB_FILE)
 	c = conn.cursor()
 	c.execute("CREATE TABLE IF NOT EXISTS {} ( \
+			ID INTEGER PRIMARY KEY AUTOINCREMENT,\
 			hashrate1m BIGINT NOT NULL,\
 			hashrate5m BIGINT NOT NULL,\
 			hashrate1hr BIGINT NOT NULL,\
@@ -66,19 +68,20 @@ def insert_data(tstamp, json_data):
 			postponed NUMERIC,\
 			herp NUMERIC NOT NULL,\
 			derp NUMERIC NOT NULL,\
-			worker TEXT)".format(TABLE_NAME))
-
-
-			
-
-
+			worker TEXT,\
+			tstamp UNIXEPOCH)".format(TABLE_NAME))
+	conn.commit()
+	conn.close()
 	#insert data
+	engine = create_engine('sqlite:////home/martin/code/usb_miner_data/{}'.format(DB_FILE))
+	df.to_sql(TABLE_NAME, con=engine, if_exists='append',index=False)
 
 
 
 def main():
-	get_pool_data()
-
+	tstamp, json_data = get_pool_data()
+	insert_data(tstamp, json_data)	
+	print("Inserted Pool Data")
 if __name__=='__main__':
 	main()
 
